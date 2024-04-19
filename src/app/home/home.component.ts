4e9c8289edf3e974cc95856a9f1d1a4d3a68ebba
d.aikema <djurreaikema@gmail.com>
2024-04-19 15:16:36 +0200

    Lesson 5 - Create the formGroup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import {Component, signal} from "@angular/core";
+import {Component, inject, signal} from "@angular/core";
 import {ModalComponent} from "../shared/ui/modal.component";
 import {Checklist} from "../shared/interfaces/checklist";
+import {FormBuilder} from "@angular/forms";
+import {FormModalComponent} from "../shared/ui/form-modal.component";
 
 @Component({
   standalone: true,
@@ -12,15 +14,34 @@ import {Checklist} from "../shared/interfaces/checklist";
     </header>
 
     <app-modal [isOpen]="!!checklistBeingEdited()">
-      <ng-template> You can't see me... yet</ng-template>
+      <ng-template>
+        <app-form-modal
+          [title]="
+                checklistBeingEdited()?.title
+                    ? checklistBeingEdited()!.title!
+                    : 'Add checklist'
+          "
+          [formGroup]="checklistForm"
+          (close)="checklistBeingEdited.set(null)"
+        />
+      </ng-template>
     </app-modal>
   `,
   imports: [
-    ModalComponent
+    ModalComponent,
+    FormModalComponent
   ]
 })
 // Responsibility:
 export default class HomeComponent {
+  // --- Dependencies
+  public formBuilder: FormBuilder = inject(FormBuilder);
+
   // Track the checklist that is currently being edited
   public checklistBeingEdited = signal<Partial<Checklist> | null>(null)
+
+  // Form for creating/editing checklists
+  public checklistForm = this.formBuilder.nonNullable.group({
+    title: [''],
+  });
 }
