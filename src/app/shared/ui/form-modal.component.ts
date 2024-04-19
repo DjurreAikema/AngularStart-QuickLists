576fd0fdd13bf0a3e7a8c55b56e36f4ab866d5d1
d.aikema <djurreaikema@gmail.com>
2024-04-19 15:07:39 +0200

    Lesson 5 - Create the form modal

---
+++ src/app/shared/ui/form-modal.component.ts
@@ -0,0 +1,39 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {KeyValuePipe} from "@angular/common";
+
+@Component({
+  standalone: true,
+  selector: 'app-form-modal',
+  template: `
+    <header>
+      <h2>{{ title }}</h2>
+      <button (click)="close.emit()">Close</button>
+    </header>
+
+    <section>
+      <form [formGroup]="formGroup" (ngSubmit)="save.emit(); close.emit()">
+        @for (control of formGroup.controls | keyvalue; track control.key) {
+          <div>
+            <label [for]="control.key">{{ control.key }}</label>
+            <input
+              [id]="control.key"
+              type="text"
+              [formControlName]="control.key"
+            />
+          </div>
+        }
+        <button type="submit">Save</button>
+      </form>
+    </section>
+  `,
+  imports: [ReactiveFormsModule, KeyValuePipe]
+})
+// Responsibility: Dumb component that TODO
+export class FormModalComponent {
+  @Input({required: true}) formGroup!: FormGroup;
+  @Input({required: true}) title!: string;
+
+  @Output() save: EventEmitter<void> = new EventEmitter<void>();
+  @Output() close: EventEmitter<void> = new EventEmitter<void>();
+}
