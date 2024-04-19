b8c92876b5b1a779061e422ff4e2799e6e70eb8e
d.aikema <djurreaikema@gmail.com>
2024-04-19 16:30:12 +0200

    Lesson 8 - Get checklist by id

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {ChecklistService} from "../shared/data-access/checklist.service";
+import {ActivatedRoute} from "@angular/router";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-checklist',
@@ -12,5 +15,15 @@ import {Component} from '@angular/core';
   styles: ``
 })
 export default class ChecklistComponent {
+  // --- Dependencies
+  public checklistService: ChecklistService = inject(ChecklistService);
+  public route: ActivatedRoute = inject(ActivatedRoute);
 
+  public params = toSignal(this.route.paramMap);
+
+  public checklist = computed(() =>
+    this.checklistService
+      .checklists()
+      .find((checklist) => checklist.id == this.params()?.get('id'))
+  );
 }
